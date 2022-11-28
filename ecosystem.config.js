b782a01fc6388e4d067176788c48153fbe8f2160
nandan vasista <nandan@nandan.local>
2022-11-28 16:10:27 +0530

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start", // because this starts the next.js server
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "serverkey.pem", //key of linux server
+      user: "ubuntu",
+      host: "13.232.61.253", //public IP
+      ref: "origin/main", //in git origin/main
+      repo: "git@github.com:nandanVasistaBH29/document-management-system.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};

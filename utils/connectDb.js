1311892cd886f815660180d440ab487820697635
nandan vasista <nandan@nandan.local>
2022-11-29 21:05:58 +0530

    danger:removed env variables and hardcoded it in connectDB

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,9 +1,10 @@
 import mysql from "mysql";
 // connection to mysql running on the same linux server
+//PLEASE KEEP THIS in ENV FILE
 export const pool = mysql.createPool({
-  host: process.env.AWS_DATABASE_ENDPOINT,
-  user: process.env.AWS_DATABASE_USER,
-  port: process.env.AWS_DATABASE_PORT,
-  password: process.env.AWS_DATABASE_PASSWORD,
-  database: process.env.AWS_DATABASE_DATABASE,
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "nandan1234",
+  database: "orgdb",
 });

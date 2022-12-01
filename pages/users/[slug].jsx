aa4d8340d175ae957c42f58b3f8b3917bfa67444
nandan vasista <nandan@nandan.local>
2022-12-01 07:31:54 +0530

    deleted service workers

--- pages/users/[slug].jsx
+++ pages/users/[slug].jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 const getQueryParams = (query) => {
   return query
@@ -13,18 +14,23 @@ const getQueryParams = (query) => {
         }, {})
     : {};
 };
+
 const User = () => {
   const [user, setUser] = useState({});
   const [files, setFiles] = useState([]);
+  const [decryptpath, setDecryptPath] = useState("");
   useEffect(() => {
     getAllDetails();
   }, []);
+
   const getAllDetails = async () => {
     const { data } = getQueryParams(window.location.search);
     const art = await JSON.parse(data);
     try {
       const res = await axios.get(
-        `/api/documents/user-docs?organizationName=${art.organizationName}&email=${art.email}`
+        `/api/documents/user-docs?organizationName=${
+          art.organizationName
+        }&email=${art.email}&encrypted=${true}`
       );
       if (res.data.err) return;
       setFiles(res.data);
@@ -33,6 +39,10 @@ const User = () => {
       console.log(err);
     }
     setUser(art);
+  };
+  const handleDecypt = async (path) => {
+    const res = await axios.post(`/api/documents/decryption`, { path });
+    console.log(res);
   };
   return (
     <div>
@@ -58,16 +68,24 @@ const User = () => {
             return (
               <>
                 <h4>{file}</h4>
+                <Link
+                  href={"/users/decrypted"}
+                  onClick={() =>
+                    handleDecypt(
+                      "/storage/" +
+                        user.organizationName +
+                        "/" +
+                        user.email +
+                        "/" +
+                        file
+                    )
+                  }
+                >
+                  Decrypt The File First
+                </Link>
                 <a
                   role="button"
-                  href={
-                    "/storage/" +
-                    user.organizationName +
-                    "/" +
-                    user.email +
-                    "/" +
-                    file
-                  }
+                  href={decryptpath}
                   download="proposed_file_name.pdf"
                 >
                   Download
--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 // connection to mysql running on the same linux server
 export const pool = mysql.createPool({
-  host: process.env.AWS_DATABASE_ENDPOINT,
-  user: process.env.AWS_DATABASE_USER,
-  port: process.env.AWS_DATABASE_PORT,
-  password: process.env.AWS_DATABASE_PASSWORD,
-  database: process.env.AWS_DATABASE_DATABASE,
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "nandan1234",
+  database: "orgdb",
 });
